b45bb493f2b60451113613e4cdf27de6f6652117
Matt Rota <matt.rota@ribbit.io>
2021-03-13 23:44:23 -0500

    finished post and implemented put (del for testing)

--- api.js
+++ api.js
@@ -11,40 +11,75 @@ router.get("/profile", async (req, res, next) => {
   }
 })
 
-router.post("/profile/create", async (req, res) => {
-  console.log(req.body)
-  const {
-    name,
-    sport,
-    gender,
-    dob,
-    description,
-    location,
-    teamName,
-    interests,
-  } = req.body
+router.post("/profile/create", async (req, res, next) => {
+  try {
+    // we'll just use the spread syntax here to fill all of the
+    // required fields. mongoose validation will catch any isseus
+    const newProfile = new Profile({ ...req.body })
 
-  const newProfile = new Profile({
-    name,
-    dob,
-    location,
-    team: teamName,
-    gender,
-    sports: sport,
-    about: description,
-    interests,
-  })
+    await newProfile.save()
 
-  await newProfile.save()
-
-  console.log(newProfile)
-  
-  // finish
-  res.send({ msg: "Success", status: 200 })
+    // finish
+    res.send({
+      msg: "Success",
+      status: 200,
+      data: newProfile._id.toString()
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.put("/profile/edit/:profileId", (req, res) => {
-  res.send("unimplemented")
+router.put("/profile/edit/:profileId", async (req, res, next) => {
+  const newProfileData = req.body
+  const profileId = req.params.profileId
+
+  try {
+    // find the profile document with the requested id
+    const oldProfile = await Profile.findById(profileId)
+
+    // make sure all fields are updated
+    // we're expecting to get all fields from the body
+    oldProfile.name = newProfileData.name
+    oldProfile.sports = newProfileData.sports
+    oldProfile.gender = newProfileData.gender
+    oldProfile.dob = newProfileData.dob
+    oldProfile.about = newProfileData.about
+    oldProfile.location = newProfileData.location
+    oldProfile.team = newProfileData.team
+    oldProfile.interests = newProfileData.interests
+
+    // save the document
+    await oldProfile.save()
+
+    res.send({
+      msg: `Profile ${profileId} updated`,
+      status: 200,
+    })
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete("/profile/delete/:profileId?", async (req, res, next) => {
+  const profileId = req.params.profileId
+  try {
+    let result
+    if (profileId) {
+      result = await Profile.deleteOne({ _id: profileId })
+    } else {
+      result = await Profile.deleteMany({})
+    }
+    
+    if (!result.ok) throw "error";
+    res.send({
+      msg: `${result.deletedCount} profiles deleted`,
+      status: 200,
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
